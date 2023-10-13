ea7ec2ca8f8bbbf22e842def0b75b803713ffc9e
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-10-13 02:31:44 -0300

    :sparkles: add task update functionality

--- src/main/java/br/com/marcosmelo/todolistjava/tasks/controllers/TaskController.java
+++ src/main/java/br/com/marcosmelo/todolistjava/tasks/controllers/TaskController.java
@@ -5,15 +5,19 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.marcosmelo.todolistjava.tasks.models.TaskModel;
 import br.com.marcosmelo.todolistjava.tasks.repositories.ITasksRepository;
+import br.com.marcosmelo.todolistjava.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -49,6 +53,24 @@ public class TaskController {
     var user_id = request.getAttribute("user_id");
     var tasks = this.tasksRepository.findByUserId((UUID) user_id);
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<String> updateTask(@RequestBody TaskModel taskModel, HttpServletRequest request,
+      @PathVariable UUID id) {
+
+    var user_id = request.getAttribute("user_id");
+    var task = this.tasksRepository.findById(id).orElse(null);
+
+    if (!task.getUserId().equals(user_id)) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    this.tasksRepository.save(task);
+
+    return ResponseEntity.ok().build();
   }
 
 }
